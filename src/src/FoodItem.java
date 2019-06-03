f66b0dc521e8ca002678ff216f25519182f9a39c
Andrew Fei <afei3@illinois.edu>
2019-06-03 17:04:33 -0500

    Added fooditem object

---
+++ src/src/FoodItem.java
@@ -0,0 +1,26 @@
+public class FoodItem {
+    private double cost;
+    private double price;
+    private String description;
+
+    public double getCost() {
+        return cost;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
+
