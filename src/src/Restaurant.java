21d162472429bfeeeb1151911b302b1b322350b6
Andrew Fei <afei3@illinois.edu>
2019-06-03 23:44:12 -0500

    Added json capabilities

--- src/src/Restaurant.java
+++ src/src/Restaurant.java
@@ -1,6 +1,13 @@
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 
 public class Restaurant {
     ArrayList<FoodItem> inventory;
 
+    public static Restaurant startFromFile(String fileName) {
+        Gson gson = new Gson();
+        Restaurant ljs = gson.fromJson(Data.getFileContentsAsString(fileName), Restaurant.class);
+        return ljs;
+    }
 }
